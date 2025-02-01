5055d9add19b9e75ec884438ba62fd8d4ae99a6c
Bruno Feiertag Martini <feiertagbruno@gmail.com>
2025-02-01 15:40:41 -0400

    antes do htmx

---
+++ a_rtchat/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import ChatmessageCreateForm
+
+@login_required
+def chat_view(request):
+    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+
+    form = ChatmessageCreateForm()
+
+    if request.method == "POST":
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            return redirect('home')
+
+    context = {
+      "chat_messages": chat_messages,
+      "form": form,
+    }
+    return render(request, "a_rtchat/chat.html", context)
